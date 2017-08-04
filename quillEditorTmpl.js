0a1fa12a4bb336c710c5f703671143f0b5a5714e
Berk BADEM <berkbadem@gmail.com>
2017-08-04 07:45:45 +0300

    added options

--- quillEditorTmpl.js
+++ quillEditorTmpl.js
@@ -14,32 +14,9 @@ const Quill = require('quill');
 import './quillEditorTmpl.html';
 
 Template.quillEditor.onRendered(function rendered() {
-	const options = {
-		modules: {
-			toolbar: [
-				  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-				  ['blockquote', 'code-block'],
-
-				  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-				  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-				  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-				  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-				  [{ 'direction': 'rtl' }],                         // text direction
-
-				  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-				  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-
-				  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-				  [{ 'font': [] }],
-				  [{ 'align': [] }],
-
-				  ['clean']                                         // remove formatting button
-			]
-		},
-		placeholder: 'Lütfen metni giriniz...',
-		theme: 'snow'
-    }
-	,	editor = new Quill('.editor', options);
+	var $input = this.data.atts.buttonClasses ? this.$('.editor') : this.$('input'); 
+	var data = this.data;                    
+	const quillEditor = new Quill('.editor', data.atts.quillOptions);
 
 	$('.editor .ql-editor').html(this.data.value);
 });
