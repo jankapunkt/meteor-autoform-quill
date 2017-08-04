eaeed8134308aad0454a7d3eb9850d4dbd5dce0c
Berk BADEM <berkbadem@gmail.com>
2017-08-04 07:14:03 +0300

    option update

--- quillEditorTmpl.js
+++ quillEditorTmpl.js
@@ -1,7 +1,7 @@
 /*
 * @Author: philipp
 * @Date:   2016-11-03 13:24:32
-* @Last Modified by:   philipp
+* @Last Modified by:   berkbadem
 * @Last Modified time: 2016-11-03 16:59:04
 */
 
@@ -17,12 +17,26 @@ Template.quillEditor.onRendered(function rendered() {
 	const options = {
 		modules: {
 			toolbar: [
-				[{ header: [1, 2, false] }],
-				['bold', 'italic', 'underline'],
-				['image', 'code-block']
+				  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+				  ['blockquote', 'code-block'],
+
+				  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+				  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+				  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+				  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+				  [{ 'direction': 'rtl' }],                         // text direction
+
+				  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+				  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+				  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+				  [{ 'font': [] }],
+				  [{ 'align': [] }],
+
+				  ['clean']                                         // remove formatting button
 			]
 		},
-		placeholder: 'Compose an epic...',
+		placeholder: 'Lütfen metni giriniz...',
 		theme: 'snow'
     }
 	,	editor = new Quill('.editor', options);
