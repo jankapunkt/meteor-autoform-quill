2ab60ecd70f2d0c24aa64dc7d4a8d0c13db0d012
Berk BADEM <berkbadem@gmail.com>
2017-08-16 14:28:45 +0300

    version update

--- package.js
+++ package.js
@@ -2,18 +2,17 @@ Package.describe({
   name: 'berkbadem:meteor-autoform-quill',
   git: 'https://github.com/berkbadem/meteor-autoform-quill',
   summary: 'Autoform Quill editor.',
-  version: '0.1.11'
+  version: '0.1.12'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.0');
+  api.versionsFrom('1.3.2.4');
   api.use([
     'ecmascript',
     'es5-shim',
     'templating',
     'reactive-var',
     'blaze-html-templates',
-    'aldeed:autoform@6.0.0',
   ], ['client', 'server']);
 
   api.addFiles([
