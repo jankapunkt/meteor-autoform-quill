26aa4cae0715c52fdbdf9b69ab68a76bada2f5e0
jankapunkt <jkuester@uni-bremen.de>
2018-06-05 15:15:35 +0200

    0.1.14 - more schema based config

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'berkbadem:meteor-autoform-quill',
   git: 'https://github.com/berkbadem/meteor-autoform-quill',
   summary: 'Autoform Quill editor.',
-  version: '0.1.13'
+  version: '0.1.14'
 });
 
 Package.onUse(function (api) {
